f9b8fd6332f7e33dbf3728b5ba94bbaa8c3047f4
zzq <i@zhangzqs.cn>
2022-12-04 14:27:04 +0800

    完善Makefile

--- common.mk
+++ common.mk
@@ -1,2 +1,9 @@
-KITE_STATIC = /kite-static
+KITE_PREFIX = /var/kite/web
+
+KITE_ABOUT_HTML = ${KITE_PREFIX}/about/index.html
+KITE_OVERRIDE_JSON = ${KITE_PREFIX}/override.json
+KITE_VERSION_TXT = ${KITE_PREFIX}/version.txt 
+KITE_UPGRADE_DIR = ${KITE_PREFIX}/upgrade/
+KITE_BULLETIN_JSON = ${KITE_PREFIX}/bulletin.json
+
 PYTHON = python
