f138a52fb5d8c5be1d8298296e8ffce2a5738ec2
zzq <i@zhangzqs.cn>
2022-12-04 16:08:55 +0800

    about页面模板化

--- about/build.py
+++ about/build.py
@@ -1,9 +1,14 @@
 from markdown import markdown
 
+with open('template.html', mode='r', encoding='utf-8') as f:
+    template = f.read()
+
 with open('index.md', mode='r', encoding='utf-8') as f:
     md = f.read()
 
-html = markdown(text=md)
+content = markdown(text=md)
+
+html = template.replace(r'{{ content }}', content)
 
 with open('index.html', mode='w', encoding='utf-8') as f:
     f.write(html)
